80e78dcb1fc55daea9c6debc59c322a5bd108795
Kay Grewe <Kay.Grewe@protonmail.com>
2024-11-04 14:39:01 +0100

    docs: mehr kommentare zur erklärung des codes hinzugefügt

--- src/main.py
+++ src/main.py
@@ -12,12 +12,13 @@ temperatures = [ {'id': 1, 'temperature': 25.5},
                  {'id': 3, 'temperature': 33.2}
 ]
 
-#Diese get route gibt uns alle daten zurück
+# Diese get route gibt uns alle daten zurück
 @app.route('/temperatures', methods=['GET'])
 def get_temperatures():
     # Wir 'serialisieren' unsere Temperaturwerte in das JSON format!
     return jsonify(temperatures)
 
+# Versucht, den spezifizierten Wert vom Nutzer aus dem Array zu holen und sie zurückzugeben
 @app.route("/temperatures/<id>", methods=['GET'])
 def get_temperature(id):
     # Wir ziehen eins von id_value ab, da unser erste element im obigen Array bei 0 startet nicht bei 1!
@@ -35,32 +36,52 @@ def get_temperature(id):
     # Wir serialisieren den angefragten temperatur wert wieder
     return jsonify(temperatures[int(id) - 1])
 
+# Die Patchroute kann benutzt werden um einen bereits vorhandenen Eintrag zu ändern! Anders als PUT muss man hierbei
+# nicht den gesamten Eintrag neuschreiben, sondern kann einzelne Werte ändern
 @app.route("/temperatures/<id>", methods=['PATCH'])
 def patch_temperature(id):
+    # Wir nehmen die ID die uns der Nutzer gesendet hat und verwandeln sie in eine Ganzzahl, dann ziehen wir
+    # eins ab da unser Array null basiert ist.
     id_value = int(id) - 1
 
+    # Wie in der GET Route testen wir ob es zu einem Over- o. Underflow kommen könnte
     if id_value >= len(temperatures):
+        # Auch hier antworten wir mit einem 404 Fehler wenn der Wert nicht exisistiert
         return '', 404
+    # Underflow?
     elif id_value - 1 < 0:
         return '', 404
     else:
+        # Sollte alles bis hierhin geklappt haben, können wir die vom Nutzer übergebenen Daten
+        # deserialisieren (JSON -> Python)
         temperature = json.loads(request.data)
-        new_temp = temperature['temperature']
-        temperatures[int(id) - 1]['temperature'] = temperature['temperature']
-
+        # new_temp beinhaltet jetzt den neuen Temperatur wert. Zur sicherheit wandeln wir den wert nochmals in einen
+        # floating point typen um!
+        new_temp = float(temperature['temperature'])
+        # Hier setzten wir den gewünschten Eintrag auf die neue Temperatur
+        temperatures[id_value]['temperature'] = new_temp
+        # Wir melden den Erfolg der Operation mit Code 201 und leiten den Nutzer auf /temperatures weiter
         return '', 201, { 'location': f'/temperatures'}
 
+# Route die benutzt wird um neue Werte hinzuzufügen, Werte werden immer an das Ende des Arrays gepackt.
 @app.route('/temperatures', methods=['POST'])
 def post_temperature():
-    print("request: ", request.data)
+    #Serialisierung des Wertes in etwas das wir nutzen können
     temperature = json.loads(request.data)
-    
+
+    #Berechne die Id des elements
     id = len(temperatures) + 1
+    #Packe die berechnete ID in unser temperatur Objekt.
     temperature['id'] = id
+    #Dieses objekt packen wir wiederum ans Ende unserer Liste.
     temperatures.append(temperature)
 
+    #Wenn wir hier landen war die Operation erfolgreich. Wir geben den Erfolgswert 201 zurück und leiten den Nutzer
+    #auf /temperatures um
     return '', 201, { 'location': f'/temperatures' }
 
 
+#Startet man die Datei wird zuerst der Code unter diesem Kommentar ausgeführt, der sogenannte Einstiegspunkt
 if __name__ == '__main__':
-    app.run(port=5000)
+    #Startet den Server so dass er auf Port 5000 auf eingehende Verbindungen wartet!
+    app.run(host="0.0.0.0", port=5000)
